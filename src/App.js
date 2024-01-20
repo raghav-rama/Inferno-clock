aa4c5969e0ede347429708fd316d0824e98462d8
raghav-rama <hackerer528@gmail.com>
2024-01-20 19:45:51 +0530

    feat: add live time

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import { version, Component } from 'inferno';
-import Logo from './logo.js';
-import './App.css';
+import { version, Component } from "inferno";
+import Logo from "./logo.js";
+import "./App.css";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    // set initial time:
+    this.state = {
+      time: Date.now(),
+    };
+  }
+
+  componentDidMount() {
+    // update time every second
+    this.timer = setInterval(() => {
+      this.setState({ time: Date.now() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    // stop when not renderable
+    clearInterval(this.timer);
+  }
+
   render() {
+    let time = new Date(this.state.time).toLocaleTimeString();
     return (
       <div className="App">
         <header className="App-header">
           <Logo width="80" height="80" />
           <p>{`Welcome to Inferno ${version}`}</p>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Time: <code>{time}</code>
           </p>
         </header>
       </div>
